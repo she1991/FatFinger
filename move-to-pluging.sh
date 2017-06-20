bd79ffdeaf7768c9c0d28bd4abe00507e2f91886
Akash Shetye <a.s@utexas.edu>
2017-06-20 13:39:26 -0500

    Starting off

---
+++ move-to-pluging.sh
@@ -0,0 +1,3 @@
+cd "/Users/akash/Library/Application Support/com.bohemiancoding.sketch3/Plugins"
+rm -rf * .*
+cp -R "/Users/akash/work/projects/HelloWorld/plugins.examples.hello-world" "/Users/akash/Library/Application Support/com.bohemiancoding.sketch3/Plugins"
