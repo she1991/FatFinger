4d332b6093eacfb8485051209870664330f93ad0
Akash <akash@Akashs-MacBook-Air.local>
2017-06-20 18:55:32 -0500

    Added preliminary files

--- move-to-pluging.sh
+++ move-to-pluging.sh
@@ -1,3 +1,3 @@
 cd "/Users/akash/Library/Application Support/com.bohemiancoding.sketch3/Plugins"
 rm -rf * .*
-cp -R "/Users/akash/work/projects/HelloWorld/plugins.examples.hello-world" "/Users/akash/Library/Application Support/com.bohemiancoding.sketch3/Plugins"
+cp -R "/Users/akash/work/project/FatFinger/FatFinger/plugins.fat-finger-test.fat-finger" "/Users/akash/Library/Application Support/com.bohemiancoding.sketch3/Plugins"
