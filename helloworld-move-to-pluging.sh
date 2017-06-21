b499b614b52c77bd76c5f00fa813b5976bf501d1
Akash <akash@Akashs-MacBook-Air.local>
2017-06-20 19:08:02 -0500

    Matched hellow worl dexample

---
+++ helloworld-move-to-pluging.sh
@@ -0,0 +1,3 @@
+cd "/Users/akash/Library/Application Support/com.bohemiancoding.sketch3/Plugins"
+rm -rf * .*
+cp -R "/Users/akash/work/project/FatFinger/FatFinger/plugins.examples.hello-world-master" "/Users/akash/Library/Application Support/com.bohemiancoding.sketch3/Plugins"
